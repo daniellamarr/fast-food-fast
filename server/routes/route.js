8327cc3453013fb1bdc3c0baea5adbb04cb8df49
daniellamarr <danielchidiebele@gmail.com>
2018-09-09 11:23:17 +0100

    [Feature #160368951] Add route to get a specific order

--- server/routes/route.js
+++ server/routes/route.js
@@ -5,6 +5,8 @@ const Route = (app) => {
     app.get('/api/v1/orders', orderControl.getAllOrders);
 
     app.post('/api/v1/orders', orderControl.placeOrder);
+
+    app.get('/api/v1/orders/:id', orderControl.getOneOrder);
 }
 
 export default Route;
