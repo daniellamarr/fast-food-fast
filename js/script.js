e045bb25989ba2dfe5d4e4ce0376ec83a9aa6bd6
daniellamarr <danielchidiebele@gmail.com>
2018-09-13 00:47:45 +0100

    [Feature #160363937] Add UI template

---
+++ js/script.js
@@ -0,0 +1,7 @@
+window.onscroll = () => {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementById("home-header").className = "hbg";
+    } else {
+        document.getElementById("home-header").className = "";
+    }
+}
