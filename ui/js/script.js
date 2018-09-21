d302d7422f26b3ed6dd11d6e41600382ca44eeec
daniellamarr <danielchidiebele@gmail.com>
2018-09-21 08:14:30 +0100

    [Feature #160363937] Add UI template

--- ui/js/script.js
+++ ui/js/script.js
@@ -1,12 +1,21 @@
-window.onscroll = () => {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        document.getElementById("home-header").className = "hbg";
-    } else {
-        document.getElementById("home-header").className = "";
+const winScroll = () => {
+    window.onscroll = () => {
+        if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+            document.getElementById("home-header").className = "hbg";
+        } else {
+            document.getElementById("home-header").className = "";
+        }
     }
 }
 
 document.getElementById('toggle').onclick = () => document.getElementById('dropdown').classList.toggle('hide');
 
 
-document.getElementById('shoppingcart-b').onclick = () => document.getElementById('shoppingcart').classList.toggle('hide');
+const shop = document.getElementsByClassName('shoppingcart-b');
+for (let i = 0; i < shop.length; i++) {
+    const shopx = shop[i];
+    shopx.onclick = () =>
+    {
+        document.getElementById('shoppingcart').classList.toggle('hide');
+    }
+}
